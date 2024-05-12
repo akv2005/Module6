d5c96d277f98673f66575cf729ad48cbae299a74
akv05 <akv0571@gmail.com>
2024-05-12 21:37:32 +0900

    Домашнее задание по теме "Множественное наследование"

--- task2.py
+++ task2.py
@@ -1,24 +1,28 @@
 class Vehicle:
      vehicle_type = None
 
+
 class Car:
     """ Автомобили """
     price = 1000000
 
     def horse_powers(self):
-        self.horse_powers = None
-        return horse_powers
+        self.horse_powers_ = None
 
     def __str__(self):
-        return 'Цена {} Мощность двигателя {}'.format(self.price, self.horse_powers)
+        return '{}:   Цена {}    Мощность двигателя {}'.format(self.vehicle_type, self.price, self.horse_powers_)
+
 
 class Nissan(Car, Vehicle):
-    vehicle_type = 'Nissan'
-    print(vehicle_type)
+    vehicle_type = 'NISSAN'
 
     def __init__(self):
         self.price = 1100000
-        self.horse_powers=200
+        self.horse_powers_= self.horse_powers()
+
+    def horse_powers(self):
+        self.horse_powers_= 200
+        return self.horse_powers_
 
 
 car_Nissan = Nissan()
