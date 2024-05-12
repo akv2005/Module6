b7152068cecfb76f11e5e43ef0d0601f9031e1ca
akv05 <akv0571@gmail.com>
2024-05-12 15:51:13 +0900

    Домашнее задание по теме "Наследование классов"

--- task1.py
+++ task1.py
@@ -3,17 +3,21 @@ class Car:
     price = 1000000
 
     def horse_powers(self):
-        self.horse_powers = None
-        return horse_powers
+        self.horse_powers_ = None
+        return horse_powers_
 
     def __str__(self):
-        return '{}:   Цена {} Мощность двигателя {}'.format(self.__class__.__name__, self.price, self.horse_powers)
+        return '{}:   Цена {}    Мощность двигателя {}'.format(self.__class__.__name__, self.price, self.horse_powers_)
 
 class Nissan(Car):
 
     def __init__(self):
         self.price = 1100000
-        self.horse_powers=200
+        self.horse_powers_= self.horse_powers()
+
+    def horse_powers(self):
+        self.horse_powers_= 200
+        return self.horse_powers_
 
 class Kia(Car):
     price = 900000
@@ -22,5 +26,6 @@ class Kia(Car):
 
 
 car_Nissan = Nissan()
+#print(Car.horse_powers())
 print(car_Nissan)
 car_Kia = Kia()
